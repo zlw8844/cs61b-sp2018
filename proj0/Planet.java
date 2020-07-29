bacb304c3c7ab7855ce556b16d4f275fdf8c7fa9
lingwei <lingweizhan@mouse-2.local>
2020-07-29 16:58:47 -0400

    G changed to private

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet{
 	public double yyVel; //Its current velocity in the y direction
 	public double mass; //Its mass
 	public String imgFileName; //The name of the file that corresponds to the image that depicts the planet (for example, jupiter.gif)
-	public static final double G = 6.67e-11;
+	private static final double G = 6.67e-11;
 	//constructor
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
 		xxPos = xP;
